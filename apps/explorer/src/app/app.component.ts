2d2c8e217913fbc0ea0162f004aa333b2f179540
Aaron Friedman <afriedman111@gmail.com>
2022-12-10 17:06:32 -0500

    tooling commands

--- apps/explorer/src/app/app.component.ts
+++ apps/explorer/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'critical-pass-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'critical-pass-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'explorer';
-}
+    title = 'explorer';
 // aasdfas fas df alksdflaksfj ;alsdkfja;lsdjfa;lsdkfj a;lsdjfa;lsdfjalsdfjas;lfjasldfjasdljfa;sldjfaslfasd;jfkl asdf af
+
+}
