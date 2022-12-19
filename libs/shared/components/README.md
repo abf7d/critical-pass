bfd74eec0537364fdc15ed7df1c9bd7fe1c253d3
Aaron Friedman <afriedman111@gmail.com>
2022-12-19 13:58:01 -0500

    fix: removed shared/activity, added shared/components

--- libs/shared/components/README.md
+++ libs/shared/components/README.md
@@ -1,7 +1,7 @@
-# shared-activity
+# shared-components
 
 This library was generated with [Nx](https://nx.dev).
 
 ## Running unit tests
 
-Run `nx test shared-activity` to execute the unit tests.
+Run `nx test shared-components` to execute the unit tests.
--- libs/shared/components/jest.config.ts
+++ libs/shared/components/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-    displayName: 'shared-activity',
+    displayName: 'shared-components',
     preset: '../../../jest.preset.js',
     setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
     globals: {
@@ -9,7 +9,7 @@ export default {
             stringifyContentPathRegex: '\\.(html|svg)$',
         },
     },
-    coverageDirectory: '../../../coverage/libs/shared/activity',
+    coverageDirectory: '../../../coverage/libs/shared/components',
     transform: {
         '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
     },
