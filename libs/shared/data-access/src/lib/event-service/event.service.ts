242a6468f328c1a5a9841e1ab5273d5de7cd21ad
Aaron Friedman <afriedman111@gmail.com>
2022-12-22 21:34:30 -0500

    fix: added event service

--- libs/shared/data-access/src/lib/event-service/event.service.ts
+++ libs/shared/data-access/src/lib/event-service/event.service.ts
@@ -6,7 +6,7 @@ import { BehaviorSubject } from 'rxjs';
 })
 export class EventService {
     public cache: Map<string, BehaviorSubject<any>> = new Map<string, BehaviorSubject<any>>();
-    public get<T>(key: string): BehaviorSubject<T>  {
+    public get<T>(key: string): BehaviorSubject<T> {
         const item = this.cache.get(key);
         if (!item) {
             const entry = new BehaviorSubject<any>(undefined);
