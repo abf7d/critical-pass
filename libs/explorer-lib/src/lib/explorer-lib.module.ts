a6f80fe949f9206d5bba25fb181977da23c21de6
Aaron Friedman <afriedman111@gmail.com>
2023-01-16 15:40:05 -0500

    fix: created provider for candeactivategaurd

--- libs/explorer-lib/src/lib/explorer-lib.module.ts
+++ libs/explorer-lib/src/lib/explorer-lib.module.ts
@@ -4,6 +4,7 @@ import { ExplorerRoutingModule } from './explorer.routes';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthHttpInterceptor } from '@critical-pass/auth';
 import { DashboardService, DASHBOARD_TOKEN, EventService, EVENT_SERVICE_TOKEN } from '@critical-pass/shared/data-access';
+import { CanDeactivateGuard } from '@critical-pass/core';
 
 @NgModule({
     imports: [CommonModule, ExplorerRoutingModule],
@@ -17,6 +18,7 @@ import { DashboardService, DASHBOARD_TOKEN, EventService, EVENT_SERVICE_TOKEN } 
         },
         { provide: DASHBOARD_TOKEN, useClass: DashboardService },
         { provide: EVENT_SERVICE_TOKEN, useClass: EventService },
+        CanDeactivateGuard,
         // { provide: 'LoggerBase', useClass: LoggerService },
         // { provide: 'HistoryFileManagerService', useClass: HistoryFileManagerService},
         // { provide: ProjectCompilerApiBase, useClass: ProjectCompilerApiService}
