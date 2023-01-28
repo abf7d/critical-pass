c6316ed6d0ec47afe966621faa21d49282db931b
Aaron Friedman <afriedman111@gmail.com>
2023-01-28 11:36:09 -0500

    fix: cleaned up

--- libs/features/profile/src/lib/profile.module.ts
+++ libs/features/profile/src/lib/profile.module.ts
@@ -1,17 +1,13 @@
 import { NgModule } from '@angular/core';
-import { ArrowBarComponent } from './sidebars/arrow-bar/arrow-bar.component';
 import { ProfileLayoutComponent } from './profile-layout/profile-layout.component';
 import { RouterModule } from '@angular/router';
-import { GraphOptionsComponent } from './components/graph-options/graph-options.component';
 import { SelectedActivityComponent } from './components/selected-activity/selected-activity.component';
-import { RiskDecompressComponent } from './components/risk-decompress/risk-decompress.component';
 import { ActionButtonsComponent } from './components/action-buttons/action-buttons.component';
 import { ShallowSBarComponent } from './sidebars/shallow-s-bar/shallow-s-bar.component';
 import { RiskBarComponent } from './sidebars/risk-bar/risk-bar.component';
 import { StackedResourcesBarComponent } from './sidebars/stacked-resources-bar/stacked-resources-bar.component';
 import { ActivityListBarComponent } from './sidebars/activity-list-bar/activity-list-bar.component';
 import { ParentProjectComponent } from './components/parent-project/parent-project.component';
-// import { SharedModule } from '../../../shared/shared.module';
 import { ProfileRoutingModule } from './profile.routes';
 import { GridButtonsComponent } from './components/grid-buttons/grid-buttons.component';
 import { ProjectMetadataComponent } from './components/project-metadata/project-metadata.component';
@@ -29,7 +25,6 @@ import {
     ShallowSSnapshotModule,
     StackedResourcesModule,
 } from '@critical-pass/charts';
-import { ToastrModule } from 'ngx-toastr';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatIconModule } from '@angular/material/icon';
@@ -38,15 +33,10 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
     declarations: [
         ProfileLayoutComponent,
-        // ArrowBarComponent,
-        // GraphOptionsComponent,
-        // SelectedActivityComponent,
-        // RiskDecompressComponent,
         ActionButtonsComponent,
         ShallowSBarComponent,
         RiskBarComponent,
@@ -66,7 +56,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
         SharedModule,
         FormsModule,
 
-        // Check if this is needed
+        // Material
         MatDatepickerModule,
         MatNativeDateModule,
         MatIconModule,
@@ -74,6 +64,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
         MatFormFieldModule,
         MatTooltipModule,
 
+        // Charts
         ArrowChartModule,
         ArrowSnapshotModule,
         ShallowSSnapshotModule,
