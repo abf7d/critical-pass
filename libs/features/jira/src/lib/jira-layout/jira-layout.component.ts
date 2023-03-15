b64dcbb02def1b48f845be9ff6f3f011e78d2f64
Aaron Friedman <afriedman111@gmail.com>
2023-03-14 21:48:34 -0400

    fix: call backend for jira token and request list of projects

--- libs/features/jira/src/lib/jira-layout/jira-layout.component.ts
+++ libs/features/jira/src/lib/jira-layout/jira-layout.component.ts
@@ -8,6 +8,7 @@ import { Activity, Project } from '@critical-pass/project/types';
 import { JiraImportMapperService, JiraProjectResult } from '@critical-pass/shared/file-management';
 import { NodeConnectorService } from '@critical-pass/project/processor';
 import { DashboardService, DASHBOARD_TOKEN } from '@critical-pass/shared/data-access';
+import urlJoin from 'url-join';
 @Component({
     selector: 'critical-pass-jira-layout',
     templateUrl: './jira-layout.component.html',
@@ -16,6 +17,7 @@ import { DashboardService, DASHBOARD_TOKEN } from '@critical-pass/shared/data-ac
 export class JiraLayoutComponent implements OnInit {
     private cloudId: string | null = null;
     public project: Project | null = null;
+    public baseUrl = environment.criticalPathApi;
     constructor(
         private httpClient: HttpClient,
         private route: ActivatedRoute,
@@ -31,19 +33,30 @@ export class JiraLayoutComponent implements OnInit {
         }
     }
     public getJiraToken(code: string): void {
-        this.httpClient
-            .post(CONST.JIRA_TOKEN_URL, {
-                grant_type: 'authorization_code',
-                code,
-                client_id: environment.jiraClientId,
-                client_secret: environment.jiraClientSecret,
-                redirect_uri: CONST.JIRA_REDIRECT_URI,
-            })
-            .subscribe((res: any) => {
-                console.log('res', res);
+        this.httpClient.get<{ access_token: string }>(urlJoin(this.baseUrl, 'account/jira-token', code)).subscribe(
+            res => {
+                console.log('token', res);
                 localStorage.setItem(CORE_CONST.JIRA_TOKEN_KEY, res.access_token);
                 this.getIssues();
-            });
+            },
+            err => {
+                console.error(err);
+            },
+        );
+        return;
+        // this.httpClient
+        //     .post(CONST.JIRA_TOKEN_URL, {
+        //         grant_type: 'authorization_code',
+        //         code,
+        //         client_id: environment.jiraClientId,
+        //         client_secret: environment.jiraClientSecret,
+        //         redirect_uri: CONST.JIRA_REDIRECT_URI,
+        //     })
+        //     .subscribe((res: any) => {
+        //         console.log('res', res);
+        //         localStorage.setItem(CORE_CONST.JIRA_TOKEN_KEY, res.access_token);
+        //         this.getIssues();
+        //     });
     }
     public getIssues(): void {
         const auth_token = localStorage.getItem(CORE_CONST.JIRA_TOKEN_KEY);
@@ -78,8 +91,62 @@ export class JiraLayoutComponent implements OnInit {
         this.project = project;
         // console.log('project', p);
     }
+    public getProjects() {
+        const auth_token = localStorage.getItem(CORE_CONST.JIRA_TOKEN_KEY);
+        if (auth_token !== null) {
+            let headers = new HttpHeaders().set('Content-Type', 'application/json').set('Authorization', `Bearer ${auth_token}`);
+            const requestOptions = { headers: headers };
+            this.httpClient
+                .get<JiraProjectResult>(`${CONST.JIRA_ISSUES_URL}${this.cloudId}/${CONST.JIRA_PROJECTS_URL}`, requestOptions)
+                .subscribe((res: any) => {
+                    const projects: JiraProject[] = res.values.map((p: any) => {
+                        return { id: p.id, key: p.key, name: p.name };
+                    });
+                    console.log('projects', projects);
+                });
+        }
+    }
 
     public toJira(): void {
         window.location.href = CONST.JIRA_LOGIN_URL;
     }
 }
+
+// 1) get a lisst of projects and create a list on the page to select a project
+// 1.1)  we need to create a custom propery in jira for the project to test saving nodes
+// 2) get a list of issues for the selected project
+// 3) create a graph from the issues
+// 4) save the graph to jira BUT first we need to create a custom propery in jira for the project
+// URL to set Project properties:
+// PUT /rest/api/3/project/{projectIdOrKey}/properties/{propertyKey}
+export interface JiraProject {
+    id: string;
+    key: string;
+    name: string;
+}
+
+/*
+const bodyData = `{
+  "number": 5,
+  "string": "string-value"
+}`;
+
+fetch('https://your-domain.atlassian.net/rest/api/3/project/{projectIdOrKey}/properties/{propertyKey}', {
+  method: 'PUT',
+  headers: {
+    'Authorization': `Basic ${Buffer.from(
+      'email@example.com:<api_token>'
+    ).toString('base64')}`,
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  },
+  body: bodyData
+})
+  .then(response => {
+    console.log(
+      `Response: ${response.status} ${response.statusText}`
+    );
+    return response.text();
+  })
+  .then(text => console.log(text))
+  .catch(err => console.error(err));*/
