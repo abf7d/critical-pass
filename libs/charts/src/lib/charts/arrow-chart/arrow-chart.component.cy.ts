247c0d0de6eb3c8ad92880020680668a79c7318d
Aaron Friedman <afriedman111@gmail.com>
2023-04-29 16:19:38 -0400

    fix: added cypress to charts library, all cypress component tests in charts are working

---
+++ libs/charts/src/lib/charts/arrow-chart/arrow-chart.component.cy.ts
@@ -0,0 +1,35 @@
+import { TestBed } from '@angular/core/testing';
+import { DashboardService, DASHBOARD_TOKEN, EventService, EVENT_SERVICE_TOKEN } from '@critical-pass/shared/data-access';
+import { ArrowChartComponent } from './arrow-chart.component';
+import { ArrowChartModule } from './arrow-chart.module';
+
+describe(ArrowChartComponent.name, () => {
+  before(function () {
+    cy.fixture('example.json').then(function (data) {
+      this['data'] = data;
+    })
+  })
+  beforeEach(() => {
+    TestBed.overrideComponent(ArrowChartComponent, {
+      add: { 
+        imports: [ArrowChartModule],
+        providers: [
+          { provide: DASHBOARD_TOKEN, useClass: DashboardService },
+          { provide: EVENT_SERVICE_TOKEN, useClass: EventService },]
+      }
+    }) 
+  })
+
+  it('renders', () => {
+     cy.mount(ArrowChartComponent, {
+           componentProperties: {
+               id:  0,
+               width:  0,
+               height:  0,
+               rebuild:  false,
+               showFastCreator:  true,
+          }
+       });
+  })
+
+})
