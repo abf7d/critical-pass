d4cefb13b9cf467c49892de686f321f6977495fe
Caleb Ukle <caleb.ukle+github@pm.me>
2023-05-01 08:41:36 -0500

    example of tasks

--- libs/charts/cypress.config.ts
+++ libs/charts/cypress.config.ts
@@ -2,5 +2,15 @@ import { defineConfig } from 'cypress';
 import { nxComponentTestingPreset } from '@nrwl/angular/plugins/component-testing';
 
 export default defineConfig({
-    component: nxComponentTestingPreset(__filename),
+    component: {
+        ...nxComponentTestingPreset(__filename),
+        setupNodeEvents(on, config) {
+            on('task', {
+                log(message) {
+                    console.log(message);
+                    return null;
+                },
+            });
+        },
+    },
 });
