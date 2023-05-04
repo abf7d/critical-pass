9be32dab683b7e8d8eb88fbe2e886a118799d232
Aaron Friedman <afriedman111@gmail.com>
2023-05-04 19:44:30 -0400

    fix: working arrow chart test rendering

--- libs/charts/src/lib/charts/arrow-chart/arrow-chart.component.cy.ts
+++ libs/charts/src/lib/charts/arrow-chart/arrow-chart.component.cy.ts
@@ -1,29 +1,26 @@
 import { TestBed } from '@angular/core/testing';
-import { ProjectCompilerService } from '@critical-pass/project/processor';
+import { DateUtilsService, NodeConnectorService, ProjectCompilerService, RiskCompilerService, StatsCalculatorService } from '@critical-pass/project/processor';
 import { Project } from '@critical-pass/project/types';
 import { DashboardService, DASHBOARD_TOKEN, EventService, EVENT_SERVICE_TOKEN } from '@critical-pass/shared/data-access';
 import { ProjectSerializerService } from '@critical-pass/shared/serializers';
-import { BehaviorSubject } from 'rxjs';
+import { ActivityValidatorService } from 'libs/project/processor/src/lib/activity-validator/activity-validator.service';
+import { CompletionCalcService } from 'libs/project/processor/src/lib/completion-calc/completion-calc.service';
+import { CriticalPathUtilsService } from 'libs/project/processor/src/lib/critical-path-utils/critical-path-utils.service';
+import { DanglingArrowService } from 'libs/project/processor/src/lib/dangling-arrow/dangling-arrow.service';
+import { GraphFactoryService } from 'libs/project/processor/src/lib/path-factories/graph-factory/graph-factory.service';
+import { ProjectValidatorService } from 'libs/project/processor/src/lib/project-validator/project-validator.service';
+import { VertexGraphBuilderService } from 'libs/project/processor/src/lib/vertex-graph-builder/vertex-graph-builder.service';
 import { ArrowChartComponent } from './arrow-chart.component';
 import { ArrowChartModule } from './arrow-chart.module';
+
 let data: Project | undefined;
-let dashboard: DashboardService;
 let serializer = new ProjectSerializerService();
-const activeProject$ = new BehaviorSubject<Project>(serializer.fromJson());
-let mockDashboard = {
-    activeProject$,
-    updateProject: (project: Project) => {
-        activeProject$.next(project);
-    },
-};
+let dashboard = configureDashboard();
 before(function () {
     cy.fixture('project.json').then(function (json) {
-        data = json;
-        cy.task('log', { message: 'This will be output to the terminal' });
-        if (data) {
-            const project = serializer.fromJson(data);
-            mockDashboard.updateProject(project);
-        }
+        data = serializer.fromJson(json);
+        dashboard.updateProject(data, true);
+        cy.task('log', {message: 'This will be output to the terminal ' + JSON.stringify(json.profile.name) + 'test'});
     });
 });
 describe(ArrowChartComponent.name, () => {
@@ -32,7 +29,7 @@ describe(ArrowChartComponent.name, () => {
             add: {
                 imports: [ArrowChartModule],
                 providers: [
-                    { provide: DASHBOARD_TOKEN, useValue: mockDashboard },
+                    { provide: DASHBOARD_TOKEN, useValue: dashboard },
                     { provide: EVENT_SERVICE_TOKEN, useClass: EventService },
                 ],
             },
@@ -43,11 +40,30 @@ describe(ArrowChartComponent.name, () => {
         cy.mount(ArrowChartComponent, {
             componentProperties: {
                 id: 0,
-                width: 0,
-                height: 0,
+                width: 1200,
+                height: 700,
                 rebuild: false,
-                showFastCreator: true,
+                showFastCreator: false,
             },
         });
     });
 });
+
+function configureDashboard(): DashboardService {
+    const graphModels = new GraphFactoryService();
+    const validator = new ProjectValidatorService();
+    const criticalPathUtils = new CriticalPathUtilsService( )
+    const graphBuilder = new VertexGraphBuilderService(graphModels);
+    const statsCalc = new StatsCalculatorService();
+    const nodeConstructor = new NodeConnectorService()
+    const dateUtils = new DateUtilsService()
+    const projectUtils = new DanglingArrowService()
+    const riskCompiler = new RiskCompilerService(validator, statsCalc, criticalPathUtils, graphBuilder)
+    const completionCalc = new CompletionCalcService()
+    const activityValidato = new ActivityValidatorService(statsCalc)
+    const projSerializer = new ProjectSerializerService();
+    const compiler = new ProjectCompilerService(nodeConstructor, dateUtils, projectUtils, riskCompiler, completionCalc, activityValidato);
+    const dashboard = new DashboardService(projSerializer, compiler);
+    return dashboard;
+
+}
