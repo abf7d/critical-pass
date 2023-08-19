9936e463e94c787c04bdfed815c627965cd96323
Aaron Friedman <afriedman111@gmail.com>
2023-08-19 16:12:58 -0400

    fix: added electron-shell app to serve electron-desktop from electron window

---
+++ apps/electron-shell/src/app/constants.ts
@@ -0,0 +1,4 @@
+export const rendererAppPort = 4200;
+export const rendererAppName = 'explorer-desktop'; // options.name.split('-')[0] + '-web'
+export const electronAppName = 'electron-shell';
+export const updateServerUrl = 'https://deployment-server-url.com'; // TODO: insert your update server url here
